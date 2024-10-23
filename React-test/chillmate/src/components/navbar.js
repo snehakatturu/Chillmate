062a0436adbb6f6dd27d5acf0e263acf305a7cdc
willi301 <will301will@gmail.comm>
2024-10-23 13:33:12 -0700

    fixed css navbar

--- React-test/chillmate/src/components/navbar.js
+++ React-test/chillmate/src/components/navbar.js
@@ -1,21 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./style.css";
+import "./ChillMateLogo.png"
 
 export default class Navbar extends React.Component {
-  
+
     render() {
       return (
-        <div className="navbar">
-            <nav>
-                <ul>
-                    <li style={{ verticalAlign: 'super' }}><a href="login.html">Login</a></li>
-                    <li style={{ verticalAlign: 'super' }}><a href="forum.html">Forum</a></li>
-                    <li style={{ verticalAlign: 'super' }}><a href="about.html">About</a></li>
-                    <li style={{ verticalAlign: 'super' }}><a href="resources.html">Resources</a></li>
-                    <li style={{ verticalAlign: 'super' }}><a href="#" id="logout">Logout</a></li>
-                </ul>
+        <div>
+          <body>
+            <nav className="navbar">
+              <ul>
+                <li>
+                  <a href="home.html">
+                    <img src={require('./ChillMateLogo.png')} alt="Logo" width="50" height="50" />
+                  </a>
+                </li>
+                <li>
+                    <a href="login.html">Login</a>
+                </li>
+                <li>
+                  <a href="forum.html">Forum</a>
+                </li>
+                <li>
+                  <a href="about.html">About</a>
+                </li>
+                <li>
+                  <a href="resources.html">Resources</a>
+                </li>
+              </ul>
             </nav>
+          </body>
         </div>
       );
     }
